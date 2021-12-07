a5947b08cd6febf05d94baa18b0fd17c7e8ac8bf
Thejaswi Amarendra <thejaswia03@gmail.com>
2021-12-07 09:20:16 +0530

    New

---
+++ Blogarithm_/client/src/pages/home/Home.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./home.css";
+import axios from "axios";
+import { useLocation } from "react-router";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts" + search);
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, [search]);
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts posts={posts} />
+      {/* <img src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"></img> */}
+      
+      </div>
+      <Sidebar />
+
+    </>
+  );
+}
